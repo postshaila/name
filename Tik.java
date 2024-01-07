2d4bf51d82023ae4be74138fa4ba99d68c6f2c7e
postshaila <138143866+postshaila@users.noreply.github.com>
2024-01-07 23:21:15 +0530

    Add files via upload

---
+++ Tik.java
@@ -0,0 +1,26 @@
+package tim;
+
+// taking input
+import java.util.*;
+public class Tik {
+	
+	int a=100;
+	int b;
+	String k;
+
+ public static void main (String [] args) {
+	Scanner kk = new Scanner(System.in);
+	System.out.println("enter");
+	int a = kk.nextInt();
+	 System.out.println("all"+a);
+
+	System.out.println("enter string");
+
+	String b = kk.next();
+		System.out.println(
+			"king......"+b);
+
+}
+ }
+
+
