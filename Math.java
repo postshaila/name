f74f479c2deb50e99593e3aa4a383886b50f8f90
postshaila <138143866+postshaila@users.noreply.github.com>
2024-01-07 23:33:16 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,18 @@
+package tim;
+
+public class Math {
+	
+	public static void addition() {
+		int a =100;
+		int b= 200;
+		int c = a+b;
+		
+		System.out.println("additon of two numbers" +c);
+	}
+	
+	public static void main (String []args) {
+		
+		addition();
+	}
+
+}
